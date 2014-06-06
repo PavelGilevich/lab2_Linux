0aa272bc8ad4e9ac3306662f289f0ca95d704db9
PavelGilevich <pavel-gilevich@mail.ru>
2014-06-06 10:52:02 +0000

    First commit

---
+++ pogoda.sh
@@ -0,0 +1,10 @@
+. $PWD/weather.cfg
+export LANG="ru_RU.CP1251"
+while true
+do
+curl -s http://www.pogoda.by > minsk
+iconv -f cp1251 -t utf8 minsk > newfile
+grep -o '<br>Температура воздуха .*°C<br>' newfile > temp
+echo "Current weather in Minsk: " $(sed 's/[^\-\+0-9.0-9]//g' temp)
+sleep $sleep_time
+done
